ee88c3887d7c24107b5678bf8830edaf3ec19c04
luizhapsouza <luizhapsouza@gmail.com>
2024-02-21 13:11:10 -0300

    testando brincar com XML

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,42 @@
-import { Router } from 'express';
+import express, { RequestHandler, Router } from 'express';
+import { parseStringPromise } from 'xml2js';
+
 
 const router = Router();
 
+// FUNCAO TESTE
+const rawTextBodyParser = express.text({ type: 'application/xml' });
 
+interface Produto {
+    cProd: string;
+    xProd: string;
+    qCom: string;
+}
 
-router.get('/test', (_, res) => { return res.status(201).json({ id: 1 }) })
+interface Detalhe {
+    prod: Produto;
+}
 
+const funcaoteste: RequestHandler = async (req, res) => {
+    try {
+        const result = await parseStringPromise(req.body, { explicitArray: false, ignoreAttrs: true });
+        const items = result.nfeProc.NFe.infNFe.det;
+        const info = items.map((det: Detalhe) => {
+            return {
+                nomeProduto: det.prod.xProd,
+                quantidade: det.prod.qCom
+            };
+        })
 
+        return res.json(info);
+    } catch (error) {
+        console.error('Erro ao parsear XML:', error);
+        return res.status(500).send('Erro ao processar o XML');
+    }
+}
+
+// /FUNCAO TESTE
+
+router.post('/test', rawTextBodyParser, funcaoteste);
 
 export { router };
